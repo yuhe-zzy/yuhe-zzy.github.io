d5f0a83d85f894732febc397416e4c8b6c5c3e1f
yuhe-zzy <60037422+yuhe-zzy@users.noreply.github.com>
2025-11-11 22:49:35 +0000

    Deploying to gh-pages from @ yuhe-zzy/yuhe-zzy.github.io@6e50671a1f7ad76094730ea73c4872a48ec874f6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "1653133307/1653133307.github.io",
+    "data-repo": "yuhe-zzy/yuhe-zzy.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
